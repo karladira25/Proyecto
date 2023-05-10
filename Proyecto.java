cb92b032984d99333e9dfa3d65bf52bf3704362d
Karla <karladira25@gmail.com>
2023-05-10 21:06:50 +0200

    Mi primer commt dn branch main

---
+++ Proyecto.java
@@ -0,0 +1,15 @@
+
+package proyecto;
+
+public class Proyecto {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int suma = num1 + num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es " + suma);
+    }
+}
+
+    
+
